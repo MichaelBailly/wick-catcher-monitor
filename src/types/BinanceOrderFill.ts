b826d4a43a9c9fcda06abbfbec82f280c1c94073
Michael Bailly <mickael.bailly@free.fr>
2022-10-01 22:01:18 +0200

    Add validator function for Binance response

--- src/types/BinanceOrderFill.ts
+++ src/types/BinanceOrderFill.ts
@@ -5,3 +5,15 @@ export type BinanceOrderFill = {
   commissionAsset: string;
   tradeId: number;
 };
+
+export function isBinanceOrderFill(obj: unknown): obj is BinanceOrderFill {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    typeof (obj as BinanceOrderFill).price === 'string' &&
+    typeof (obj as BinanceOrderFill).qty === 'string' &&
+    typeof (obj as BinanceOrderFill).commission === 'string' &&
+    typeof (obj as BinanceOrderFill).commissionAsset === 'string' &&
+    typeof (obj as BinanceOrderFill).tradeId === 'number'
+  );
+}
--- src/types/BinanceOrderResponse.ts
+++ src/types/BinanceOrderResponse.ts
@@ -1,4 +1,4 @@
-import { BinanceOrderFill } from './BinanceOrderFill';
+import { BinanceOrderFill, isBinanceOrderFill } from './BinanceOrderFill';
 
 export type BinanceOrderResponse = {
   symbol: string;
@@ -16,3 +16,27 @@ export type BinanceOrderResponse = {
   side: string;
   fills: BinanceOrderFill[];
 };
+
+export function isBinanceOrderResponse(
+  obj: unknown
+): obj is BinanceOrderResponse {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    typeof (obj as BinanceOrderResponse).symbol === 'string' &&
+    typeof (obj as BinanceOrderResponse).orderId === 'number' &&
+    typeof (obj as BinanceOrderResponse).orderListId === 'number' &&
+    typeof (obj as BinanceOrderResponse).clientOrderId === 'string' &&
+    typeof (obj as BinanceOrderResponse).transactTime === 'number' &&
+    typeof (obj as BinanceOrderResponse).price === 'string' &&
+    typeof (obj as BinanceOrderResponse).origQty === 'string' &&
+    typeof (obj as BinanceOrderResponse).executedQty === 'string' &&
+    typeof (obj as BinanceOrderResponse).cummulativeQuoteQty === 'string' &&
+    typeof (obj as BinanceOrderResponse).status === 'string' &&
+    typeof (obj as BinanceOrderResponse).timeInForce === 'string' &&
+    typeof (obj as BinanceOrderResponse).type === 'string' &&
+    typeof (obj as BinanceOrderResponse).side === 'string' &&
+    Array.isArray((obj as BinanceOrderResponse).fills) &&
+    (obj as BinanceOrderResponse).fills.every(isBinanceOrderFill)
+  );
+}
