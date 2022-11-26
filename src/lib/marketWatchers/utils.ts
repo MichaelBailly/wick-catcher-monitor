6c888e7c6fbb1aa0dc5f5090e89171f355cbebb5
Michael Bailly <mickael.bailly@free.fr>
2022-11-26 18:45:20 +0100

    Do not include quoteAmountin configuration line identifier

--- src/lib/marketWatchers/utils.ts
+++ src/lib/marketWatchers/utils.ts
@@ -1,6 +1,9 @@
 export function confLine(opts: Record<string, any>): string {
   let conf = [];
-  for (const [_, value] of Object.entries(opts)) {
+  for (const [k, value] of Object.entries(opts)) {
+    if (k === 'quoetAmount') {
+      continue;
+    }
     if (typeof value === 'boolean') {
       conf.push(value ? 'true' : 'false');
     } else {
