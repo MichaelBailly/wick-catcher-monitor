399b571d3b643451f9fe01d94fe805baafbf8ea4
Michael Bailly <mickael.bailly@free.fr>
2023-01-07 17:23:12 +0100

    Fix wait for response body decoding in case of error

--- src/lib/tradeDriver/shared.ts
+++ src/lib/tradeDriver/shared.ts
@@ -1,25 +1,32 @@
 import { format } from 'date-fns';
+import debug from 'debug';
 import { writeFile } from 'fs/promises';
 import { RECORDER_FILE_PATH } from '../../config';
 import { BinanceOrderResponse } from '../../types/BinanceOrderResponse';
 import { BinanceTransactionError } from '../../types/BinanceTransactionError';
 
+const d = debug('lib:tradeDriver:sellTransaction:shared');
+
 export async function onTransactionError(
   error: BinanceTransactionError | Error
 ) {
+  d('start onTransactionError');
   const errorFilename = `${RECORDER_FILE_PATH}/binance-transaction-${format(
     new Date(),
     'yyyyMMddHHmmssSS'
   )}.err`;
   const fileContents = [];
   if (error instanceof BinanceTransactionError && error.response) {
+    d('error is BinanceTransactionError');
     fileContents.push(`response.ok: ${error.response.ok}`);
     fileContents.push(`response.status: ${error.response.status}`);
     fileContents.push(`response.statusText: ${error.response.statusText}`);
     fileContents.push(`response.bodyUsed: ${error.response.bodyUsed}`);
     try {
+      d('reading response body');
       const body = await error.response.text();
       fileContents.push(`response.body: ${body}`);
+      d('setting error.body');
       error.body = body;
     } catch (e) {
       fileContents.push(`response.body: Unable to read response.body: ${e}`);
