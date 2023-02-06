f19b0ab07b21d44496901b97b26614ee657abb71
BAILLY Michael <mickael.bailly@free.fr>
2023-02-06 11:26:03 +0100

    Use quantity as string for sell operation

--- src/lib/tradeDriver/sellTransaction.ts
+++ src/lib/tradeDriver/sellTransaction.ts
@@ -3,7 +3,7 @@ import debug from 'debug';
 import { writeFile } from 'fs/promises';
 import { RECORDER_FILE_PATH } from '../../config';
 
-import { getSymbol, sell as binanceSell } from '../../exchanges/binance';
+import { sell as binanceSell } from '../../exchanges/binance';
 import { BinanceTransactionError } from '../../types/BinanceTransactionError';
 import { TradeDriver } from '../tradeDriver';
 import { onTransactionError, recordBinanceTransactionResponse } from './shared';
@@ -23,117 +23,17 @@ export async function sell(driver: TradeDriver) {
       attempts++;
     }
   }
-  log('End of first sell loop. Now trying alternative strategies');
-  log(
-    'lastError instanceof BinanceTransactionError? %o',
-    lastError instanceof BinanceTransactionError
-  );
-  log('lastError.body? %o', (lastError as BinanceTransactionError).body);
-  log(
-    'typeof lastError.body? %s',
-    typeof (lastError as BinanceTransactionError).body
-  );
-  if (!(lastError instanceof BinanceTransactionError)) {
-    throw new Error(`Failed to sell after ${attempts} attempts`);
-  }
-
-  // try to decrease by one
-  log('Trying "pass string" strategy');
-  const transactionResponse = await sellTentativePassString(driver, lastError);
-  if (transactionResponse) {
-    return transactionResponse;
-  }
-  attempts++;
   throw new Error(`Failed to sell after ${attempts} attempts`);
 }
 
-async function sellTentativePassString(
-  driver: TradeDriver,
-  previousError: BinanceTransactionError
-) {
-  if (!previousError.body) {
-    throw new Error('No body found in previous error');
-  }
-
-  let errorBody;
-
-  try {
-    errorBody = JSON.parse(previousError.body);
-  } catch (e) {
-    log('Failed to JSON.parse Binance error');
-  }
-  if (errorBody?.code === -1013) {
-    log('-1013 LOT_SIZE error detected. Trying "decrease by one" strategy');
-    if (!driver.binanceBuyTransaction) {
-      throw new Error('No buy transaction found');
-    }
-    const newAmount = driver.binanceBuyTransaction.executedQty;
-    log('forced amount: %d', newAmount);
-    try {
-      return await sellTentative(driver, newAmount, 'passString');
-    } catch (e) {
-      log(`Error: %s sell attempt failed: %o`, driver.pair, e);
-      throw e;
-    }
-  }
-}
-
-async function sellTentativeDecByOne(
-  driver: TradeDriver,
-  previousError: BinanceTransactionError
-) {
-  if (!previousError.body) {
-    throw new Error('No body found in previous error');
-  }
-
-  let errorBody;
-
-  try {
-    errorBody = JSON.parse(previousError.body);
-  } catch (e) {
-    log('Failed to JSON.parse Binance error');
-  }
-  if (errorBody?.code === -1013) {
-    log('-1013 LOT_SIZE error detected. Trying "decrease by one" strategy');
-    if (!driver.binanceBuyTransaction) {
-      throw new Error('No buy transaction found');
-    }
-    const newAmount = decreaseByOne(driver.binanceBuyTransaction?.executedQty);
-    log('forced amount: %d', newAmount);
-    try {
-      return await sellTentative(driver, newAmount);
-    } catch (e) {
-      log(`Error: %s sell attempt failed: %o`, driver.pair, e);
-      throw e;
-    }
-  }
-}
-
-export async function sellTentative(
-  driver: TradeDriver,
-  forcedAmount: number | string | undefined = undefined,
-  strategy: string | undefined = undefined
-) {
+export async function sellTentative(driver: TradeDriver) {
   if (!driver.binanceBuyTransaction) {
     throw new Error('No buy transaction found');
   }
 
-  const amount =
-    forcedAmount !== undefined
-      ? forcedAmount
-      : getSellAmount(
-          parseFloat(driver.binanceBuyTransaction.executedQty),
-          driver.pair
-        );
+  const amount = driver.binanceBuyTransaction.executedQty;
 
-  recordTransactionArgs(
-    driver.pair,
-    driver.binanceBuyTransaction.executedQty,
-    forcedAmount !== undefined
-      ? forcedAmount
-      : parseFloat(driver.binanceBuyTransaction.executedQty),
-    amount
-  );
+  recordTransactionArgs(driver.pair, driver.binanceBuyTransaction.executedQty);
 
   let response;
   try {
@@ -163,49 +63,14 @@ export async function sellTentative(
     price,
     response,
     doneTimestamp: Date.now(),
-    strategy,
   };
 }
 
-function getSellAmount(qty: number, pair: string) {
-  const symbol = getSymbol(pair);
-  const power = symbol?.baseAssetPrecision || 8;
-  const factor = 10 ** power;
-  return Math.floor(qty * factor) / factor;
-}
-
-export async function recordTransactionArgs(
-  pair: string,
-  qty: string,
-  executedQty: number | string,
-  amount: number | string
-) {
+export async function recordTransactionArgs(pair: string, qty: string) {
   const filename = `${RECORDER_FILE_PATH}/binance-transaction-args-${format(
     new Date(),
     'yyyyMMddHHmm'
   )}.txt`;
-  const fileContents = [
-    `pair: ${pair}`,
-    `executedQty: ${qty}`,
-    `executedQty (float): ${executedQty}`,
-    `amount: ${amount}`,
-  ];
+  const fileContents = [`pair: ${pair}`, `executedQty: ${qty}`];
   writeFile(filename, fileContents.join('\n'), 'utf8');
-}
-
-function decreaseByOne(amount: string) {
-  if (amount.indexOf('.') >= 0) {
-    let newAmount = amount;
-    const lastZeroIndex = amount.lastIndexOf('0');
-    if (lastZeroIndex === -1 || lastZeroIndex < amount.length - 1) {
-      newAmount =
-        newAmount.substring(0, newAmount.length - 1) +
-        `${parseFloat(newAmount[newAmount.length - 1]) - 1}`;
-    }
-    return parseFloat(newAmount);
-  } else {
-    let newAmount = parseFloat(amount);
-    newAmount--;
-    return newAmount;
-  }
 }
