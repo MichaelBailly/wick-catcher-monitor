d256a945e5611be7fc542654c54c93f037c09a99
Michael Bailly <mickael.bailly@free.fr>
2023-11-27 13:10:09 +0100

    Set stronger function guard for MarketWatcher

--- src/types/MarketWatcher.ts
+++ src/types/MarketWatcher.ts
@@ -17,6 +17,63 @@ export type MarketWatcher = {
   getTradeDriverOpts: () => TradeDriverOpts;
 };
 
-export function isMarketWatcher(obj: any): obj is MarketWatcher {
-  return obj.pair && obj.d && obj.e && obj.minutes && obj.historySize;
+export function isMarketWatcher(obj: unknown): obj is MarketWatcher {
+  if (typeof obj !== 'object' || obj === null) {
+    return false;
+  }
+
+  const obj2 = obj as MarketWatcher;
+
+  if (typeof obj2.pair !== 'string') {
+    return false;
+  }
+
+  if (typeof obj2.d !== 'function') {
+    return false;
+  }
+
+  if (typeof obj2.e !== 'function') {
+    return false;
+  }
+
+  if (!Array.isArray(obj2.minutes)) {
+    return false;
+  }
+
+  if (typeof obj2.historySize !== 'number') {
+    return false;
+  }
+
+  if (
+    typeof obj2.followBtcTrend !== 'boolean' &&
+    typeof obj2.followBtcTrend !== 'number'
+  ) {
+    return false;
+  }
+
+  if (typeof obj2.onKlineMessage !== 'function') {
+    return false;
+  }
+
+  if (typeof obj2.detectFlashWick !== 'function') {
+    return false;
+  }
+
+  if (typeof obj2.getHistory !== 'function') {
+    return false;
+  }
+
+  if (typeof obj2.getConfLine !== 'function') {
+    return false;
+  }
+
+  if (typeof obj2.getConfData !== 'function') {
+    return false;
+  }
+
+  if (typeof obj2.getTradeDriverOpts !== 'function') {
+    return false;
+  }
+
+  return true;
 }
